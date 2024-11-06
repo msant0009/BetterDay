4b829e64e46ed72f40e52bab06a5671280fde97d
Mark Santoro <mark_santoro@comcast.net>
2024-11-06 13:46:33 -0800

    edits up to timestamp 28:48

--- A Better Day Workshop/Views/ThingsView.swift
+++ A Better Day Workshop/Views/ThingsView.swift
@@ -6,10 +6,34 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ThingsView: View {
+    
+    @Query(filter: #Predicate<Thing> {$0.isHidden == false})private var things: [Thing]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Things")
+                .font(.largeTitle)
+                .bold()
+            
+            Text("These are the things that make you feel positive and uplifted")
+            
+            List (things){thing in
+                Text(thing.title)
+            }
+            Spacer()
+            
+            Button("Add New Thing") {
+                // todo: show sheet to add thing
+            }
+            .buttonStyle(.borderedProminent)
+            .frame(maxWidth: .infinity,alignment: .center)
+        }
+        Spacer()
+        
     }
 }
 
